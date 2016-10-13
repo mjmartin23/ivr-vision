d2f4f681f51c1bb993cdee83c295200284a0b8bb
Matt Martin <mjmartin@colby.edu>
2016-10-13 13:07:48 +0100

    updated norm_rgb to give a binary image output for bkg subtraction

--- main.m
+++ main.m
@@ -1,11 +1,15 @@
 % file for running main script
 
-I = imread('simpler/02.jpg');
+I = imread('simpler/05.jpg');
 
 % will need to find a way to programatically determine this
 numObjects = 12;
 
+I = norm_rgb(I);
+
 [out,centres,groups] = mean_shift(I,5,25,1000);
+
+figure(),imshow(out);
 
 disp('mean shifted');
 
--- norm_rgb.m
+++ norm_rgb.m
@@ -1,9 +1,34 @@
 function [out] = norm_rgb(I)
+    figure(1),imshow(I);
     red = double(I(:,:,1));
     green = double(I(:,:,2));
     blue = double(I(:,:,3));
     denom = red + green + blue;
     dub = double(I);
-    out = uint8(255.*(dub./denom));
-end
+    normed = uint8(255.*(dub./denom));
     
+    %imtool(normed);
+    
+    [rows,cols,~] = size(normed);
+    rmid=median(reshape(normed(:,:,1),[1,rows*cols]));
+    gmid=median(reshape(normed(:,:,2),[1,rows*cols]));
+    bmid=median(reshape(normed(:,:,3),[1,rows*cols]));
+    
+    out = zeros(rows,cols);
+    for i = 1:rows
+        for j = 1:cols
+             if ((normed(i,j,1) < rmid+10) && ... 
+                 (normed(i,j,2) < gmid+5) && ...
+                 (normed(i,j,3) < bmid+5))
+%            if (abs(normed(i,j,1) - normed(i,j,2)) + abs(normed(i,j,1) - normed(i,j,3)) + ...
+%                    abs(normed(i,j,2) - normed(i,j,3)) < 20)
+                out(i,j) = 0;
+            else
+                out(i,j) = 1;
+            end
+        end
+    end
+    
+    figure(),imshow(out);
+                
+end
