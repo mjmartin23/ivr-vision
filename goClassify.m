25b79dab8be6974dc72000d5a4f95436e75cb2f3
Matt Martin <mjmartin@colby.edu>
2016-10-25 19:55:44 +0100

    fixing go

---
+++ goClassify.m
@@ -0,0 +1,12 @@
+function [count] = goClassify(features,N,Means,Invcors,Aprioris,clusters,v)
+    X=features.normal_features;
+    num = size(X,1);
+    count = zeros(num,N);
+        for i= 1:num
+            x=X(i,:);
+            feats.surf = features.SURF_features(i);
+            feats.fast = features.FAST_features(i);
+            [c,~] = classify(x,N,Means,Invcors,0,Aprioris,feats,clusters,v);
+            count(i,c) = 1;
+        end
+    end
